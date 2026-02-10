44dca9836e1a7a560ee84c9211e53a22d5eb0e6d
chayuearn-00 <chayudaearn@gmail.com>
2026-02-10 14:23:56 +0700

    just add flex for components.

---
+++ frontend/src/components/TextCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col-3 ">
+        <div class="flex items-center gap-4.5 mb-11">
+            <img src="@/assets/icons/logo_person.png" class="w-10" />
+            <p>#001</p>
+            <p class="text-primary">USERNAME</p>
+        </div>
+        <div class="text-7xl">
+            <p>Designed</p>
+            <p>to stay</p>
+            <p>essential</p>
+        </div>
+    </div>
+</template>
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,11 +1,23 @@
-<script setup lang="ts">
+<script setup >
 import NavBar from '../components/NavBar.vue';
+import Footer from '../components/Footer.vue';
+import InformationCard from '../components/InformationCard.vue';
+import TextCard from '../components/TextCard.vue';
 
 </script>
 
 <template>
+    <NavBar class="fixed w-full"/>
     <div class="container overflow-hidden">
-        <NavBar class="fixed w-full"/>
-        <img src="@/assets/Images/images_test.jpg" class="h-800" />
+        <div class="flex flex-cols-2 place-content-center min-h-screen items-center justify-center max-w-full ">
+            <div class="grid-cols-2 ">
+                <TextCard />
+                <InformationCard />
+            </div>
+            <div class="footer_position">
+                <Footer />
+            </div>
+        </div>
     </div>
 </template> 
+    <!-- <img src="@/assets/Images/images_test.jpg" class="h-800" /> -->
