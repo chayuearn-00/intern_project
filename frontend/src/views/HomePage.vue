221f37c99d3d020068629efad3120372a4eccb1b
chayuearn-00 <chayudaearn@gmail.com>
2026-02-12 15:40:48 +0700

    edit perfect position

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -8,16 +8,19 @@ import TextCard from '../components/TextCard.vue';
 
 <template>
     <NavBar class="fixed w-full"/>
-    <div class="container overflow-hidden">
-        <div class="max-w-[90%] lg:max-w-[70%] mx-auto">
-            <div class="flex flex-cols-2 w-full place-content-center min-h-screen items-center m-auto ">
+    <div class=" bg-white container overflow-hidden">
+        <div class="max-w-[90%] lg:max-w-[70%] mx-auto pt-28">
+            <div class="flex flex-col w-full place-content-center min-h-screen items-center m-auto ">
                 <div class="grid grid-cols-2 w-full place-content-between">
                     <TextCard />
                     <InformationCard />
                 </div>
-                <!-- <div class="footer_position">
-                    <Footer />
-                </div> -->
+                <div class="w-full mt-20">
+                    <p class="text-center">Over 4M+ connected with us!</p>
+                    <div class="footer_position">
+                        <Footer />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
