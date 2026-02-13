304212002b60d13b7b681b7c04e032a92af0a1e7
chayuearn-00 <chayudaearn@gmail.com>
2026-02-13 14:19:49 +0700

    edit layout+color

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -8,18 +8,16 @@ import TextCard from '../components/TextCard.vue';
 
 <template>
     <NavBar class="fixed w-full"/>
-    <div class="container overflow-hidden">
-        <div class="max-w-full lg:max-w-[80%] mx-auto pt-0">
-            <div class="flex flex-col w-full place-content-center min-h-screen items-center m-auto ">
-                <div class="lg:grid lg:grid-cols-2 lg:gap-8 w-full place-content-between">
-                    <TextCard />
-                    <InformationCard class="lg:block hidden"/>
-                </div>
-                <div class="flex flex-col w-full mt-8 lg:mt-20">
-                    <p class="text-center text-xs">Over 4M+ connected with us!</p>
-                    <div class="footer_position">
-                        <Footer />
-                    </div>
+    <div class="container bg-white overflow-hidden">
+        <div class="homepage-layout">
+            <div class="lg:grid lg:grid-cols-2 lg:gap-8 w-full place-content-between">
+                <TextCard />
+                <InformationCard class="lg:block hidden"/>
+            </div>
+            <div class="flex flex-col w-full mt-8 lg:mt-20">
+                <p class="text-center text-xs">Over 4M+ connected with us!</p>
+                <div class="footer_position">
+                    <Footer />
                 </div>
             </div>
         </div>
