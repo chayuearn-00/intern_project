c2a0a8f7e017f5d793db81fae67d5eb53c664464
chayuearn-00 <chayudaearn@gmail.com>
2026-02-20 08:47:30 +0700

    change wrong word

--- frontend/src/views/DashboardPage.vue
+++ frontend/src/views/DashboardPage.vue
@@ -1,27 +1,26 @@
 <template>
-    <NavBar />
-    <div class="container bg-background ">
-        <div class="flex felx-col py-9 px-20 h-screen">
-            <div class="flex w-full place-content-between h-min">
-                <ConclutionCard />
-                <ConclutionCard />
-                <ConclutionCard />
-                <ConclutionCard />
-            </div>
-            <div class="flex max-w-full">
-                <Chart />
-                <div class="flex flex-col">
-                    <DataTable />
-                    <DataLine />
+    <div class="dashboard-card gap-7.5">
+        <div class="flex place-content-between">
+            <div class="flex gap-4 justify-center">
+                <div class="relative bg-primary w-8.5 h-8.5 rounded-lg">
+                    <img :src="Realtime" class="absolute inset-0 m-auto"/>
                 </div>
+                <p class="text-black font-bold text-xl">Real-time Sensors</p>
+            </div>
+            <div class="bg-primary h-5 rounded-lg">
+                <p class="text-white text-xs font-semibold px-2.5 py-1">4 modules</p>
             </div>
         </div>
+        <div className="grid grid-cols-2 grid-rows-2 gap-7.5">
+            <SensorCard />
+            <SensorCard />
+            <SensorCard />
+            <SensorCard />
+        </div>
     </div>
-
 </template>
 
 <script setup>
-import NavBar from '../components/NavBar.vue';
-import ConclutionCard from '../components/dashboard/ConclutionCard.vue';
-
+import Realtime from '../../assets/icons/dashboard/Realtime.svg';
+import SensorCard from './SensorCard.vue';
 </script>
