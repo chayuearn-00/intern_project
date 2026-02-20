6463ad8a05e1ecc8f5bea751ae2f352243222a9f
chayuearn-00 <chayudaearn@gmail.com>
2026-02-20 11:31:05 +0700

    create card

---
+++ frontend/src/components/dashboard/Status.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="px-2.5 py-1 border border-status-bsafe bg-secondary rounded-3xl right-0 text-white">
+        <span class="m-0">SAVE</span>
+</div>
+</template>
+
+<script setup>
+
+</script>
